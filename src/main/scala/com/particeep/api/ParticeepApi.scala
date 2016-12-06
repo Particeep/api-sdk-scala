891151e214333a4082e8f0eaa296baa8eae019cf
Adrien <adrien.crovetto@gmail.com>
2016-12-06 14:17:41 +0100

    add helper

---
+++ src/main/scala/com/particeep/api/ParticeepApi.scala
@@ -0,0 +1,21 @@
+package com.particeep.api
+
+import com.particeep.api.core.{ApiClient, ApiCredential}
+
+/**
+ * This is a high level builder for api client. We may add new
+ * Usage :
+ *   val client = ParticeepApi.test("your_api_key", "your_api_secret")
+ */
+object ParticeepApi {
+
+  private val last_version = "1"
+
+  def test(key: String, secret: String): ApiClient = {
+    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoClient with UserClient
+  }
+
+  def prod(key: String, secret: String): ApiClient = {
+    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoClient with UserClient
+  }
+}
