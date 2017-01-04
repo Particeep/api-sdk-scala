198ac89e4f7f31100a72b2a2d142121119478155
thomasAvril <thomas.avril83@gmail.com>
2017-01-04 16:57:28 +0100

    create all Kyc related models

---
+++ src/main/scala/com/particeep/api/models/kyc/Kyc.scala
@@ -0,0 +1,19 @@
+package com.particeep.api.models.kyc
+
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
+import com.particeep.api.models.enums.KycType.{ID_CARD, KycType}
+import play.api.libs.json.Json
+
+case class Kyc(
+  doc_type:   KycType               = ID_CARD,
+  created_at: Option[ZonedDateTime] = None,
+  update_at:  Option[ZonedDateTime] = None,
+  urls:       Seq[String]           = Seq()
+)
+
+object Kyc {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
+  val format = Json.format[Kyc]
+}
---
+++ src/main/scala/com/particeep/api/models/kyc/KycGroup.scala
@@ -0,0 +1,17 @@
+package com.particeep.api.models.kyc
+
+import com.particeep.api.models.enums.KycStatus.{CREATED, KycStatus}
+import play.api.libs.json.Json
+
+case class KycGroup(
+  owner_id:   String    = "",
+  owner_type: String    = "",
+  status:     KycStatus = CREATED,
+  docs:       Seq[Kyc]  = Seq()
+)
+
+object KycGroup {
+  implicit val kyc_format = Kyc.format
+
+  val format = Json.format[KycGroup]
+}
