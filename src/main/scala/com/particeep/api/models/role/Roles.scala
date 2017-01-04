b9a95cd2cdfcf73754922caf077dc0e015944d5f
thomasAvril <thomas.avril83@gmail.com>
2017-01-04 18:29:22 +0100

    RoleClient + format

--- src/main/scala/com/particeep/api/models/role/Roles.scala
+++ src/main/scala/com/particeep/api/models/role/Roles.scala
@@ -3,9 +3,9 @@ package com.particeep.api.models.role
 import play.api.libs.json.Json
 
 case class Roles(
-                    user_id: String       = "",
-                    roles:   List[String] = List()
-                  )
+  user_id: String       = "",
+  roles:   List[String] = List()
+)
 
 object Roles {
   val format = Json.format[Roles]
