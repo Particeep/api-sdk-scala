b8aa0eb432a929bbaa296524aaf7170087613e4f
thomasAvril <thomas.avril83@gmail.com>
2017-01-10 17:42:25 +0100

    add FormCapability to ParticeepApi

--- src/main/scala/com/particeep/api/ParticeepApi.scala
+++ src/main/scala/com/particeep/api/ParticeepApi.scala
@@ -12,10 +12,10 @@ object ParticeepApi {
   private val last_version = "1"
 
   def test(key: String, secret: String): ApiClient = {
-    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoCapability with UserCapability with WalletCapability with KycCapability with RoleCapability with DocumentCapability
+    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoCapability with UserCapability with WalletCapability with KycCapability with RoleCapability with DocumentCapability with FormCapability
   }
 
   def prod(key: String, secret: String): ApiClient = {
-    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoCapability with UserCapability with WalletCapability with KycCapability with RoleCapability with DocumentCapability
+    new ApiClient("https://test-api.particeep.com", ApiCredential(key, secret), last_version) with InfoCapability with UserCapability with WalletCapability with KycCapability with RoleCapability with DocumentCapability with FormCapability
   }
 }
