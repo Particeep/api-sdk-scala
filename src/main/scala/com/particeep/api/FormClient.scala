03caabafa308b5200d5c40b3fecc03b9c62a8fba
thomasAvril <thomas.avril83@gmail.com>
2017-01-10 16:53:59 +0100

    method to add answers

--- src/main/scala/com/particeep/api/FormClient.scala
+++ src/main/scala/com/particeep/api/FormClient.scala
@@ -2,7 +2,7 @@ package com.particeep.api
 
 import com.particeep.api.core.{ResponseParser, WSClient}
 import com.particeep.api.models.ErrorResult
-import com.particeep.api.models.form.{Form, FormCreation, FormEdition, SimpleForm}
+import com.particeep.api.models.form._
 import play.api.libs.json.Json
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -20,6 +20,8 @@ class FormClient(ws: WSClient) extends ResponseParser {
   implicit val format_simple = SimpleForm.format
   implicit val format_creation = FormCreation.format
   implicit val format_edition = FormEdition.format
+  implicit val format_answer = Answer.format
+  implicit val format_answer_creation = AnswerCreation.format
 
   def all(timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Seq[SimpleForm]]] = {
     ws.url(s"$endPoint/all", timeout).get().map(parse[Seq[SimpleForm]])
@@ -39,5 +41,9 @@ class FormClient(ws: WSClient) extends ResponseParser {
 
   def update(id: String, form_edition: FormEdition, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Form]] = {
     ws.url(s"$endPoint/$id").post(Json.toJson(form_edition)).map(parse[Form])
+  }
+
+  def answer(user_id: String, answer_creations: Seq[AnswerCreation], timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Seq[Answer]]] = {
+    ws.url(s"$endPoint/answer/$user_id").put(Json.toJson(answer_creations)).map(parse[Seq[Answer]])
   }
 }
