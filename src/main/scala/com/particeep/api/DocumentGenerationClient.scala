64b06b95a8828c1f3502025f5f41dc9326afd0c5
thomasAvril <thomas.avril83@gmail.com>
2017-01-12 15:08:37 +0100

    update document-generation client to match API

--- src/main/scala/com/particeep/api/DocumentGenerationClient.scala
+++ src/main/scala/com/particeep/api/DocumentGenerationClient.scala
@@ -18,7 +18,7 @@ class DocumentGenerationClient(ws: WSClient) extends ResponseParser {
   private val endPoint: String = "/document-generation"
   implicit val format_generation = DocumentGeneration.format
 
-  def generation(document_generation: DocumentGeneration, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Array[Byte]]] = {
-    ws.url(s"$endPoint", timeout).post(Json.toJson(document_generation)).map(parse[Array[Byte]])
+  def generation(document_generation: DocumentGeneration, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Stream[Byte]]] = {
+    ws.url(s"$endPoint", timeout).post(Json.toJson(document_generation)).map(parse[Stream[Byte]])
   }
 }
