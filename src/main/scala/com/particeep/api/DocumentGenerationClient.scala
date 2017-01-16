a52820a07b62b36f85113f974e7a6b2ff417bd89
thomasAvril <thomas.avril83@gmail.com>
2017-01-16 14:27:19 +0100

    add method in doc gen client that uploads the generated document

--- src/main/scala/com/particeep/api/DocumentGenerationClient.scala
+++ src/main/scala/com/particeep/api/DocumentGenerationClient.scala
@@ -2,6 +2,7 @@ package com.particeep.api
 
 import com.particeep.api.core.{ResponseParser, WSClient}
 import com.particeep.api.models.ErrorResult
+import com.particeep.api.models.document.Document
 import com.particeep.api.models.document_generation.DocumentGeneration
 import play.api.libs.json.Json
 
@@ -17,8 +18,13 @@ class DocumentGenerationClient(ws: WSClient) extends ResponseParser {
 
   private val endPoint: String = "/document-generation"
   implicit val format_generation = DocumentGeneration.format
+  implicit val format_document = Document.format
 
   def generation(document_generation: DocumentGeneration, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Stream[Byte]]] = {
     ws.url(s"$endPoint", timeout).post(Json.toJson(document_generation)).map(parse[Stream[Byte]])
+  }
+
+  def generationAndUpload(document_generation: DocumentGeneration, owner_id: String, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Document]] = {
+    ws.url(s"$endPoint/upload/$owner_id", timeout).post(Json.toJson(document_generation)).map(parse[Document])
   }
 }
