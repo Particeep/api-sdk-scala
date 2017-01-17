2cad548e8bfe2437d75f8531359b2c078a9274ad
thomasAvril <thomas.avril83@gmail.com>
2017-01-17 17:01:14 +0100

    create related models

---
+++ src/main/scala/com/particeep/api/models/enums/FundraiseStatus.scala
@@ -0,0 +1,20 @@
+package com.particeep.api.models.enums
+
+import scala.language.implicitConversions
+
+object FundraiseStatus {
+
+  sealed abstract class FundraiseStatus extends Enum
+  case object INIT extends FundraiseStatus { val name: String = "INIT" }
+  case object UNDER_REVIEW extends FundraiseStatus { val name: String = "UNDER_REVIEW" }
+  case object RUNNING extends FundraiseStatus { val name: String = "RUNNING" }
+  case object SUCCEEDED extends FundraiseStatus { val name: String = "SUCCEEDED" }
+  case object REFUNDED extends FundraiseStatus { val name: String = "REFUNDED" }
+  case object DELETED extends FundraiseStatus { val name: String = "DELETED" }
+
+  object FundraiseStatus extends EnumHelper[FundraiseStatus] {
+    def values: Set[FundraiseStatus] = Set(INIT, UNDER_REVIEW, RUNNING, SUCCEEDED, REFUNDED, DELETED)
+
+    implicit def stringToFundraiseStatus(value: String): FundraiseStatus = get(value).getOrElse(INIT)
+  }
+}
---
+++ src/main/scala/com/particeep/api/models/fundraise/loan/RepaymentInfo.scala
@@ -0,0 +1,19 @@
+package com.particeep.api.models.fundraise.loan
+
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
+import com.particeep.api.models.enums.CalculatorType.CalculatorType
+import play.api.libs.json.Json
+
+case class RepaymentInfo(
+  val method:               CalculatorType,
+  val repayment_frequency:  Option[Int],
+  val repayment_start_date: ZonedDateTime,
+  val amount:               Option[Int]    = None
+)
+
+object RepaymentInfo {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
+  val format = Json.format[RepaymentInfo]
+}
