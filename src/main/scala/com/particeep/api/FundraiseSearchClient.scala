ac37d98fc27c74f82de2bc2a23da36fd7e22e4c0
thomasAvril <thomas.avril83@gmail.com>
2017-01-18 10:48:21 +0100

    aff nbProjectsByCategory in FundraiseSearchClient

--- src/main/scala/com/particeep/api/FundraiseSearchClient.scala
+++ src/main/scala/com/particeep/api/FundraiseSearchClient.scala
@@ -2,7 +2,7 @@ package com.particeep.api
 
 import com.particeep.api.core.{ResponseParser, WSClient}
 import com.particeep.api.models.{ErrorResult, PaginatedSequence}
-import com.particeep.api.models.fundraise.{FundraiseData, FundraiseSearch}
+import com.particeep.api.models.fundraise.{FundraiseData, FundraiseSearch, NbProjectsByCategory}
 import com.particeep.api.utils.LangUtils
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -17,11 +17,19 @@ class FundraiseSearchClient(ws: WSClient) extends ResponseParser {
 
   private val endPoint: String = "/fundraises"
   implicit val format = FundraiseData.format
+  implicit val project_by_category_format = NbProjectsByCategory.format
 
   def search(criteria: FundraiseSearch, timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, PaginatedSequence[FundraiseData]]] = {
     ws.url(s"$endPoint/search", timeout)
       .withQueryString(LangUtils.productToQueryString(criteria): _*)
       .get
       .map(parse[PaginatedSequence[FundraiseData]])
+  }
+
+  def nbProjectsByActivityDomain(categories: Seq[String], timeout: Long = -1)(implicit exec: ExecutionContext): Future[Either[ErrorResult, Seq[NbProjectsByCategory]]] = {
+    ws.url(s"$endPoint/info/categories", timeout)
+      .withQueryString("categories" -> categories.mkString(","))
+      .get()
+      .map(parse[Seq[NbProjectsByCategory]])
   }
 }
