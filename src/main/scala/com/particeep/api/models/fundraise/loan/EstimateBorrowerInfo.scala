e9cd0fe5e0a87bfeea5e4657dc826889a2e81095
Adrien <adrien.crovetto@gmail.com>
2017-03-17 15:47:29 +0100

    update code for estimate lending

---
+++ src/main/scala/com/particeep/api/models/fundraise/loan/EstimateBorrowerInfo.scala
@@ -0,0 +1,25 @@
+package com.particeep.api.models.fundraise.loan
+
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
+import com.particeep.api.models.enums.CalculatorType.CalculatorType
+import org.joda.time.DateTime
+import play.api.libs.json.Json
+
+case class EstimateBorrowerInfo(
+  amount:               Int,
+  term:                 Int,
+  rate:                 Double,
+  tax_rate:             Double,
+  amount_min:           Int,
+  method:               CalculatorType,
+  repayment_frequency:  Int,
+  repayment_start_date: DateTime,
+  does_pay_taxes:       Boolean        = false
+)
+
+object EstimateBorrowerInfo {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
+  val format = Json.format[EstimateBorrowerInfo]
+}
