3716c7553efec713bbba7dbe1dc22763b08ff2f7
thomasAvril <thomas.avril83@gmail.com>
2017-04-05 11:30:02 +0200

    add WebHookKyc model

---
+++ src/main/scala/com/particeep/api/models/kyc/WebHookKyc.scala
@@ -0,0 +1,20 @@
+package com.particeep.api.models.kyc
+
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
+import com.particeep.api.models.enums.KycType.{ ID_CARD, KycType }
+import play.api.libs.json.Json
+
+case class WebHookKyc(
+  owner_id:   String                = "",
+  doc_type:   KycType               = ID_CARD,
+  created_at: Option[ZonedDateTime] = None,
+  update_at:  Option[ZonedDateTime] = None,
+  urls:       Seq[String]           = Seq()
+)
+
+object WebHookKyc {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
+  val format = Json.format[WebHookKyc]
+}
