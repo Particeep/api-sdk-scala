495f32f184a9381a1f0e384c51d7aded62ce4db1
thomasAvril <thomas.avril83@gmail.com>
2017-04-20 10:00:54 +0200

    add CouponConstant in CalculatorType

--- src/main/scala/com/particeep/api/models/enums/CalculatorType.scala
+++ src/main/scala/com/particeep/api/models/enums/CalculatorType.scala
@@ -4,9 +4,10 @@ object CalculatorType {
 
   sealed abstract class CalculatorType extends Enum
   case object Constant extends CalculatorType { val name: String = "Constant" }
+  case object CouponConstant extends CalculatorType { val name: String = "CouponConstant" }
   case object InFine extends CalculatorType { val name: String = "InFine" }
 
   object CalculatorType extends EnumHelper[CalculatorType] {
-    def values: Set[CalculatorType] = Set(Constant, InFine)
+    def values: Set[CalculatorType] = Set(Constant, CouponConstant, InFine)
   }
 }
