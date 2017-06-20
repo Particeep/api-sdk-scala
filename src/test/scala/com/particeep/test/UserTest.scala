a469a67915de658e5031e52a4ebd62c3357c2930
Adrien <adrien.crovetto@gmail.com>
2017-06-20 10:47:09 +0200

    new tests

--- src/test/scala/com/particeep/test/UserTest.scala
+++ src/test/scala/com/particeep/test/UserTest.scala
@@ -5,6 +5,7 @@ import com.particeep.api.models.user.User
 import com.particeep.api.core.ApiClient
 import com.particeep.api.models.ErrorResult
 import com.particeep.api.UserCapability
+import com.particeep.api.ParticeepApi
 import org.scalatest._
 
 import scala.concurrent.{ Await, Future }
@@ -13,12 +14,53 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class UserTest extends FlatSpec with Matchers {
 
-  "the api client" should "load user by id" in {
+  "the api client" should "load user by id with direct credentials" in {
+
+    val user_id = "bf5788e8-9756-4d18-8b0f-100d7fba17a2"
+    val ws = new ApiClient(ConfigTest.baseUrl, ConfigTest.version, Some(ConfigTest.credential)) with UserCapability
+    val rez_f: Future[Either[ErrorResult, User]] = ws.user.byId(user_id)
+
+    val rez = Await.result(rez_f, 10 seconds)
+    rez.isRight shouldBe true
+
+    val user = rez.right.get
+    user.id shouldBe user_id
+  }
+
+  "the api client" should "load user by id with custom credentials" in {
 
     val user_id = "bf5788e8-9756-4d18-8b0f-100d7fba17a2"
     val ws = new ApiClient(ConfigTest.baseUrl, ConfigTest.version) with UserCapability
-    implicit val credentials = ConfigTest.credential
+    val credentials = ConfigTest.credential
+    val rez_f: Future[Either[ErrorResult, User]] = ws.user(credentials).byId(user_id)
+
+    val rez = Await.result(rez_f, 10 seconds)
+    rez.isRight shouldBe true
+
+    val user = rez.right.get
+    user.id shouldBe user_id
+  }
+
+  "the api client" should "load user by id with helper" in {
+
+    val user_id = "bf5788e8-9756-4d18-8b0f-100d7fba17a2"
+    val ws = ParticeepApi.test(ConfigTest.credential.apiKey, ConfigTest.credential.apiSecret)
     val rez_f: Future[Either[ErrorResult, User]] = ws.user.byId(user_id)
+
+    val rez = Await.result(rez_f, 10 seconds)
+    rez.isRight shouldBe true
+
+    val user = rez.right.get
+    user.id shouldBe user_id
+  }
+
+  "the api client" should "load user by id with helper and overload credentials" in {
+
+    val user_id = "bf5788e8-9756-4d18-8b0f-100d7fba17a2"
+    val ws = ParticeepApi.test(ConfigTest.credential.apiKey, ConfigTest.credential.apiSecret)
+    val credentials = ConfigTest.credential
+
+    val rez_f: Future[Either[ErrorResult, User]] = ws.user(credentials).byId(user_id)
 
     val rez = Await.result(rez_f, 10 seconds)
     rez.isRight shouldBe true
