028abbcf9d903f95af878bfd1bdfa9693c3c1b6d
Alexandre Annic <alexandre.annic@particeep.com>
2017-07-06 11:12:19 +0200

    Prevent to update parent target on update (2)

--- src/main/scala/com/particeep/api/models/form/edition/QuestionEdition.scala
+++ src/main/scala/com/particeep/api/models/form/edition/QuestionEdition.scala
@@ -4,7 +4,6 @@ import com.particeep.api.models.enums.QuestionType.QuestionType
 import play.api.libs.json.Json
 
 case class QuestionEdition(
-  section_id:    Option[String],
   label:         Option[String],
   question_type: Option[QuestionType],
   required:      Option[Boolean],
--- src/main/scala/com/particeep/api/models/form/edition/SectionEdition.scala
+++ src/main/scala/com/particeep/api/models/form/edition/SectionEdition.scala
@@ -3,7 +3,6 @@ package com.particeep.api.models.form.edition
 import play.api.libs.json.Json
 
 case class SectionEdition(
-  form_id:     Option[String],
   name:        Option[String],
   description: Option[String],
   index:       Option[Int]
