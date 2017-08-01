2b162427cb5626d714fc67bc0fab849df51dd51a
lasne_noe <noe.lasne@particeep.com>
2017-08-01 12:51:50 +0200

    TransactionWallet search

---
+++ src/main/scala/com/particeep/api/models/wallet/TransactionWalletSearch.scala
@@ -0,0 +1,22 @@
+package com.particeep.api.models.wallet
+
+import java.time.ZonedDateTime
+
+/**
+ * Created by Noe on 01/08/2017.
+ */
+case class TransactionWalletSearch(
+  wallet_id:          Option[String]        = None,
+  debited_wallet_id:  Option[String]        = None,
+  credited_wallet_id: Option[String]        = None,
+  debited_amount:     Option[Int]           = None,
+  debited_amount_min: Option[Int]           = None,
+  debited_amount_max: Option[Int]           = None,
+  createdAfter:       Option[ZonedDateTime] = None,
+  createdBefore:      Option[ZonedDateTime] = None,
+  currency:           Option[String]        = None,
+  status:             Option[String]        = None,
+  tag:                Option[String]        = None,
+  limit:              Option[Int]           = None,
+  offset:             Option[Int]           = None
+)
