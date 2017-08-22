868860048775df1cb20f7ac7b6d6a3be63fb9b77
Alexandre Annic <alexandre.annic@particeep.com>
2017-08-22 14:40:40 +0200

    Add missing question type DATE

--- src/main/scala/com/particeep/api/models/enums/QuestionType.scala
+++ src/main/scala/com/particeep/api/models/enums/QuestionType.scala
@@ -7,6 +7,7 @@ object QuestionType {
   sealed abstract class QuestionType extends Enum
   case object TEXT extends QuestionType { val name: String = "TEXT" }
   case object LONGTEXT extends QuestionType { val name: String = "LONGTEXT" }
+  case object DATE extends QuestionType { val name: String = "DATE" }
   case object RADIO extends QuestionType { val name: String = "RADIO" }
   case object SELECT extends QuestionType { val name: String = "SELECT" }
   case object CHECKBOX extends QuestionType { val name: String = "CHECKBOX" }
@@ -14,7 +15,7 @@ object QuestionType {
   case object LABEL extends QuestionType { val name: String = "LABEL" }
 
   object QuestionType extends EnumHelper[QuestionType] {
-    def values: Set[QuestionType] = Set(TEXT, LONGTEXT, RADIO, SELECT, CHECKBOX, DOCUMENT, LABEL)
+    def values: Set[QuestionType] = Set(TEXT, LONGTEXT, DATE, RADIO, SELECT, CHECKBOX, DOCUMENT, LABEL)
 
     implicit def stringToQuestionType(value: String): QuestionType = get(value).getOrElse(TEXT)
 
