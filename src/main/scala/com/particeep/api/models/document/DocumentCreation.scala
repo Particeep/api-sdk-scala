f83dbae557539a48c1616f76c3b613cf25dba52a
Noe Lasne <noe.lasne@particeep.com>
2017-11-09 14:48:44 +0100

    Add custom on document

--- src/main/scala/com/particeep/api/models/document/DocumentCreation.scala
+++ src/main/scala/com/particeep/api/models/document/DocumentCreation.scala
@@ -1,18 +1,20 @@
 package com.particeep.api.models.document
 
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsObject, Json }
 
 /**
  * Created by Noe on 04/01/2017.
  */
 case class DocumentCreation(
-  target_id:              Option[String]  = None,
-  target_type:            Option[String]  = None,
-  description:            Option[String]  = None,
-  name:                   Option[String]  = None,
-  path:                   Option[String]  = None,
-  locked:                 Option[Boolean] = None,
-  override_existing_file: Option[Boolean] = None
+  target_id:              Option[String]   = None,
+  target_type:            Option[String]   = None,
+  description:            Option[String]   = None,
+  name:                   Option[String]   = None,
+  path:                   Option[String]   = None,
+  locked:                 Option[Boolean]  = None,
+  override_existing_file: Option[Boolean]  = None,
+  tag:                    Option[String]   = None,
+  custom:                 Option[JsObject] = None
 )
 
 object DocumentCreation {
