61e9fa3ddca68e1937fd68b248967a974f54bb78
Adrien <adrien.crovetto@gmail.com>
2017-12-12 12:39:01 +0100

    add builder

--- src/main/scala/com/particeep/api/core/ApiClient.scala
+++ src/main/scala/com/particeep/api/core/ApiClient.scala
@@ -1,5 +1,7 @@
 package com.particeep.api.core
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import com.ning.http.client.multipart.{ FilePart, Part }
 import com.ning.http.client.{ AsyncHttpClient, Response }
 import play.api.libs.Files.TemporaryFile
@@ -30,16 +32,6 @@ trait BaseClient {
 
 }
 
-/**
- * usage
- * val ws = new ApiClient(
- *   "https://api.particeep.com",
- *   "1",
- *   creds
- * ) with UserCapability
- *
- * val result:Future[Either[JsError, Info]] = ws.user.byId("some_id")
- */
 class ApiClient(val ws: StandaloneAhcWSClient, val baseUrl: String, val version: String, val credentials: Option[ApiCredential] = None) extends WSClient with BaseClient with WithSecurtiy {
 
   def url(path: String, timeOut: Long = -1)(implicit exec: ExecutionContext, credentials: ApiCredential): StandaloneWSRequest = {
@@ -55,11 +47,73 @@ class ApiClient(val ws: StandaloneAhcWSClient, val baseUrl: String, val version:
       new FilePart("document", documentFilePart.ref.file, documentFilePart.contentType.getOrElse("application/octet-stream"))
     )
     bodyParts.map(builder.addBodyPart(_))
-    Future { client.executeRequest(builder.build()).get }
+    Future { client.executeRequest(builder.build()).get }(exec)
   }
 
   private[this] def urlFileUpload(path: String, client: AsyncHttpClient, timeOut: Long)(implicit exec: ExecutionContext, credentials: ApiCredential): AsyncHttpClient#BoundRequestBuilder = {
     val postBuilder = client.preparePost(s"$baseUrl/v$version$path")
     secure(postBuilder, credentials, timeOut)
+  }
+}
+
+/**
+ * usage
+ * val ws = new ApiClient(
+ *   "https://api.particeep.com",
+ *   "1",
+ *   credential
+ * ) with UserCapability
+ *
+ * val result:Future[Either[JsError, Info]] = ws.user.byId("some_id")
+ */
+object ApiClient {
+
+  /**
+   * This constructor is safe to be call multiple times
+   */
+  def apply(
+    ws:          StandaloneAhcWSClient,
+    baseUrl:     String,
+    version:     String,
+    credentials: Option[ApiCredential]
+  ): ApiClient = {
+    new ApiClient(ws, baseUrl, version, credentials)
+  }
+
+  /**
+   * This constructor is safe to be call multiple times
+   */
+  def apply(
+    system:      ActorSystem,
+    baseUrl:     String,
+    version:     String,
+    credentials: Option[ApiCredential]
+  ): ApiClient = {
+    implicit val mat = ActorMaterializer()(system)
+    val ws = StandaloneAhcWSClient()
+    new ApiClient(ws, baseUrl, version, credentials)
+  }
+
+  /**
+   * This constructor create its own actor system, don't call it multiple times ! Instead make it a singleton
+   *
+   * usage
+   * val ws = new ApiClient(
+   *   "https://api.particeep.com",
+   *   "1",
+   *   credential
+   * ) with UserCapability
+   *
+   * val result:Future[Either[JsError, Info]] = ws.user.byId("some_id")
+   */
+  def apply(
+    baseUrl:     String,
+    version:     String,
+    credentials: Option[ApiCredential] = None
+  ): ApiClient = {
+    implicit val system = ActorSystem("particeep-api-client")
+    implicit val mat = ActorMaterializer()
+    val ws = StandaloneAhcWSClient()
+    new ApiClient(ws, baseUrl, version, credentials)
   }
 }
--- src/test/scala/com/particeep/test/BaseTest.scala
+++ src/test/scala/com/particeep/test/BaseTest.scala
@@ -6,6 +6,6 @@ import org.scalatest._
 
 trait BaseTest extends FlatSpec with Matchers {
 
-  implicit val system:ActorSystem = ActorSystem("test-system")
+  implicit val system: ActorSystem = ActorSystem("test-system")
   implicit val mat: akka.stream.Materializer = ActorMaterializer()
 }
