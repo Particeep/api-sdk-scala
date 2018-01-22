7256a1ba349effb75f24bf4964673aa522429cc1
Najim <najimParticeep>
2018-01-22 21:00:01 +0100

    add multiple role in one request

---
+++ src/main/scala/com/particeep/api/models/role/RolesCreation.scala
@@ -0,0 +1,18 @@
+package com.particeep.api.models.role
+
+import play.api.libs.json.Json
+
+case class RolesCreation(
+  id:                 String         = "",
+  user_id:            String         = "",
+  role_name:          String         = "",
+  RolesCreation_name: String         = "",
+  target_id:          Option[String] = None,
+  target_type:        Option[String] = None,
+  tag:                Option[String] = None
+
+)
+
+object RolesCreation {
+  val format = Json.format[RolesCreation]
+}
