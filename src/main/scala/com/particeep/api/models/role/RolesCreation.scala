76ba5b4403b5bc64559a6d007228c644f0ef2119
Najim <najimParticeep>
2018-01-23 15:37:50 +0100

    remove useless variable

--- src/main/scala/com/particeep/api/models/role/RolesCreation.scala
+++ src/main/scala/com/particeep/api/models/role/RolesCreation.scala
@@ -3,13 +3,12 @@ package com.particeep.api.models.role
 import play.api.libs.json.Json
 
 case class RolesCreation(
-  id:                 String         = "",
-  user_id:            String         = "",
-  role_name:          String         = "",
-  RolesCreation_name: String         = "",
-  target_id:          Option[String] = None,
-  target_type:        Option[String] = None,
-  tag:                Option[String] = None
+  id:          String         = "",
+  user_id:     String         = "",
+  role_name:   String         = "",
+  target_id:   Option[String] = None,
+  target_type: Option[String] = None,
+  tag:         Option[String] = None
 
 )
 
