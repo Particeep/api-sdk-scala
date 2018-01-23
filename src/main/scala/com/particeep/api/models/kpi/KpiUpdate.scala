2dc6339a525da16ea5059fa494591e96313a1381
Najim <najimParticeep>
2018-01-23 15:43:56 +0100

    clean stuff

--- src/main/scala/com/particeep/api/models/kpi/KpiUpdate.scala
+++ src/main/scala/com/particeep/api/models/kpi/KpiUpdate.scala
@@ -5,11 +5,11 @@ import play.api.libs.json.{ Json, JsObject }
 case class KpiUpdate(
   name:        String,
   user_id:     String,
-  target_id:   Option[String],
-  target_type: Option[String],
-  tag:         Option[String],
-  custom:      Option[JsObject],
-  values:      Option[Seq[KpiValue]]
+  target_id:   Option[String]        = None,
+  target_type: Option[String]        = None,
+  tag:         Option[String]        = None,
+  custom:      Option[JsObject]      = None,
+  values:      Option[Seq[KpiValue]] = None
 )
 
 object KpiUpdate {
