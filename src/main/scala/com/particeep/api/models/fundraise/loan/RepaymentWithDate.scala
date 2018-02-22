07a702447322d79097978e80c870c5cdf0b7ce0b
Noe Lasne <noe.lasne@particeep.com>
2018-02-22 15:34:33 +0100

    Better result for repayment detail

--- src/main/scala/com/particeep/api/models/fundraise/loan/RepaymentWithDate.scala
+++ src/main/scala/com/particeep/api/models/fundraise/loan/RepaymentWithDate.scala
@@ -3,7 +3,8 @@ package com.particeep.api.models.fundraise.loan
 import java.time.ZonedDateTime
 
 import com.particeep.api.core.Formatter
-import play.api.libs.json.Json
+import com.particeep.api.models.user.User
+import play.api.libs.json._
 
 case class Repayment(
   capital:                Int,
@@ -23,5 +24,18 @@ case class RepaymentWithDate(
 object RepaymentWithDate {
   implicit val date_format = Formatter.ZonedDateTimeWrites
   private[this] implicit val repayment_format = Json.format[Repayment]
-  val format = Json.format[RepaymentWithDate]
+  private[this] implicit val user_format = Json.format[User]
+  implicit val format = Json.format[RepaymentWithDate]
+
+  val repayment_with_date_and_user_format: Format[(RepaymentWithDate, User)] = new Format[(RepaymentWithDate, User)] {
+    def reads(json: JsValue): JsResult[(RepaymentWithDate, User)] = JsSuccess((
+      RepaymentWithDate(
+        date = (json \ "date").as[ZonedDateTime],
+        repayment = (json \ "repayment").as[Repayment]
+      ),
+      (json \ "user").as[User]
+    ))
+    def writes(repaymentWithDateAndUser: (RepaymentWithDate, User)): JsValue =
+      Json.toJson(repaymentWithDateAndUser._1).as[JsObject] + ("user" -> Json.toJson(repaymentWithDateAndUser._2))
+  }
 }
