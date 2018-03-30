ab7985aee4d5658f2c7c916a27bda3b9e14cc601
Romain Loth <romain.loth@particeep.com>
2018-03-30 09:39:46 +0200

    Revert "ZonedDateTime for ScoringEvaluationSearch" (not merged via develop) This reverts commit ae92d64de3f16e5aba693b111bb89adeabc8dcb6.

--- src/main/scala/com/particeep/api/models/scoring_metrics/ScoringEvaluationSearch.scala
+++ src/main/scala/com/particeep/api/models/scoring_metrics/ScoringEvaluationSearch.scala
@@ -1,17 +1,17 @@
 package com.particeep.api.models.scoring_metrics
 
-import java.time.ZonedDateTime
 import com.particeep.api.core.Formatter
+import org.joda.time.DateTime
 import play.api.libs.json.Json
 
 case class ScoringEvaluationSearch(
-  created_after:  Option[ZonedDateTime] = None,
-  created_before: Option[ZonedDateTime] = None,
-  score:          Option[Long]          = None,
-  metric_id:      Option[String]        = None,
-  target_id:      Option[String]        = None,
-  target_type:    Option[String]        = None,
-  tag:            Option[String]        = None
+  created_after:  Option[DateTime] = None,
+  created_before: Option[DateTime] = None,
+  score:          Option[Long]     = None,
+  metric_id:      Option[String]   = None,
+  target_id:      Option[String]   = None,
+  target_type:    Option[String]   = None,
+  tag:            Option[String]   = None
 )
 
 object ScoringEvaluationSearch {
