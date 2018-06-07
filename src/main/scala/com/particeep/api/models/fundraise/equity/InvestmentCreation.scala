79a5bf5dd8fe5c3a2a3313770938e407ce4ba93c
Noe Lasne <noe.lasne@particeep.com>
2018-06-07 16:19:32 +0200

    Allow to set created_at when creating a Transaction

--- src/main/scala/com/particeep/api/models/fundraise/equity/InvestmentCreation.scala
+++ src/main/scala/com/particeep/api/models/fundraise/equity/InvestmentCreation.scala
@@ -1,12 +1,17 @@
 package com.particeep.api.models.fundraise.equity
 
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
 import play.api.libs.json.Json
 
 case class InvestmentCreation(
-  user_id: String,
-  amount:  Int
+  user_id:    String,
+  amount:     Int,
+  created_at: Option[ZonedDateTime]
 )
 
 object InvestmentCreation {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
   val format = Json.format[InvestmentCreation]
 }
--- src/main/scala/com/particeep/api/models/transaction/TransactionCreation.scala
+++ src/main/scala/com/particeep/api/models/transaction/TransactionCreation.scala
@@ -1,5 +1,8 @@
 package com.particeep.api.models.transaction
 
+import java.time.ZonedDateTime
+
+import com.particeep.api.core.Formatter
 import com.particeep.api.models.enums.Currency.{ Currency, EUR }
 import play.api.libs.json.{ JsObject, Json }
 
@@ -7,22 +10,24 @@ import play.api.libs.json.{ JsObject, Json }
  * Created by Noe on 03/04/2017.
  */
 case class TransactionCreation(
-  issuer_id:      String           = "",
-  issuer_type:    String           = "",
-  recipient_id:   String           = "",
-  recipient_type: String           = "",
-  fundraise_id:   Option[String]   = None,
-  fundraise_type: Option[String]   = None,
-  item_id:        Option[String]   = None,
-  item_type:      Option[String]   = None,
-  amount:         Int              = 0,
-  fees:           Int              = 0,
-  currency:       Currency         = EUR,
-  comment:        Option[String]   = None,
-  tag:            Option[String]   = None,
-  custom:         Option[JsObject] = None
+  created_at:     Option[ZonedDateTime] = None,
+  issuer_id:      String                = "",
+  issuer_type:    String                = "",
+  recipient_id:   String                = "",
+  recipient_type: String                = "",
+  fundraise_id:   Option[String]        = None,
+  fundraise_type: Option[String]        = None,
+  item_id:        Option[String]        = None,
+  item_type:      Option[String]        = None,
+  amount:         Int                   = 0,
+  fees:           Int                   = 0,
+  currency:       Currency              = EUR,
+  comment:        Option[String]        = None,
+  tag:            Option[String]        = None,
+  custom:         Option[JsObject]      = None
 )
 
 object TransactionCreation {
+  implicit val date_format = Formatter.ZonedDateTimeWrites
   val format = Json.format[TransactionCreation]
 }
