d59bdc2b9762da6952083d23bd11295e587fcbc5
Najim <najim.baby@particeep.com>
2018-10-23 13:16:28 +0000

    scoring metrics update search method with criteria

---
+++ src/main/scala/com/particeep/api/models/scoring_metrics/ScoringMetricSearch.scala
@@ -0,0 +1,9 @@
+package com.particeep.api.models.scoring_metrics
+
+import java.time.ZonedDateTime
+case class ScoringMetricSearch(
+  created_after:  Option[ZonedDateTime] = None,
+  created_before: Option[ZonedDateTime] = None,
+  formula_name:   Option[String]        = None,
+  tag:            Option[String]        = None
+)
